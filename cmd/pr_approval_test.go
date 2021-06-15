52df5393476114b96dbb741be2463e2d5f14ea07
Marti Butler <5116631+martibutler@users.noreply.github.com>
2021-06-15 08:47:00 +0100

    PSEC-1208 Updated tests for prApproval function

--- cmd/pr_approval_test.go
+++ cmd/pr_approval_test.go
@@ -4,13 +4,38 @@ import (
 	"github-admin-tool/graphqlclient"
 	"reflect"
 	"testing"
+
+	"github.com/pkg/errors"
 )
 
+func mockedUpdatePrApprovalBranchProtection(branchProtectionRuleID string, client *graphqlclient.Client) error {
+	return nil
+}
+
+func mockedCreatePrApprovalBranchProtection(repositoryID, branchName string, client *graphqlclient.Client) error {
+	return nil
+}
+
+func mockedUpdatePrApprovalBranchProtectionError(branchProtectionRuleID string, client *graphqlclient.Client) error {
+	return errors.New("Test update error")
+}
+
+func mockedCreatePrApprovalBranchProtectionError(repositoryID, branchName string, client *graphqlclient.Client) error {
+	return errors.New("Test create error")
+}
+
 func Test_applyPrApproval(t *testing.T) {
+	prApprovalUpdate = mockedUpdatePrApprovalBranchProtection
+	defer func() { signingUpdate = updatePrApprovalBranchProtection }()
+
+	prApprovalCreate = mockedCreatePrApprovalBranchProtection
+	defer func() { signingCreate = createPrApprovalBranchProtection }()
+
 	type args struct {
 		repoSearchResult map[string]RepositoriesNodeList
 		client           *graphqlclient.Client
 	}
+
 	tests := []struct {
 		name         string
 		args         args
@@ -18,6 +43,7 @@ func Test_applyPrApproval(t *testing.T) {
 		wantCreated  []string
 		wantInfo     []string
 		wantProblems []string
+		returnError  bool
 	}{
 		{
 			name: "applyPrApprovalNoDefaultBranch",
@@ -31,7 +57,6 @@ func Test_applyPrApproval(t *testing.T) {
 			},
 			wantInfo: []string{"No default branch for org/some-repo-name"},
 		},
-
 		{
 			name: "applyPrApprovalUpdate",
 			args: args{
@@ -39,8 +64,8 @@ func Test_applyPrApproval(t *testing.T) {
 					"repo0": {
 						ID:            "repoIdTEST",
 						NameWithOwner: "org/some-repo-name",
-						DefaultBranchRef: DefaultBranchRef {
-							Name: "some-branch-name",	 
+						DefaultBranchRef: DefaultBranchRef{
+							Name: "some-branch-name",
 						},
 						BranchProtectionRules: BranchProtectionRules{
 							Nodes: []BranchProtectionRulesNodesList{{
@@ -52,15 +77,67 @@ func Test_applyPrApproval(t *testing.T) {
 			},
 			wantModified: []string{"org/some-repo-name"},
 		},
-
-
-
+		{
+			name: "applyPrApprovalCreate",
+			args: args{
+				repoSearchResult: map[string]RepositoriesNodeList{
+					"repo0": {
+						ID:            "repoIdTEST",
+						NameWithOwner: "org/some-repo-name",
+						DefaultBranchRef: DefaultBranchRef{
+							Name: "some-branch-name",
+						},
+					},
+				},
+			},
+			wantCreated: []string{"org/some-repo-name"},
+		},
+		{
+			name: "applyPrApprovalUpdateError",
+			args: args{
+				repoSearchResult: map[string]RepositoriesNodeList{
+					"repo0": {
+						ID:            "repoIdTEST",
+						NameWithOwner: "org/some-repo-name",
+						DefaultBranchRef: DefaultBranchRef{
+							Name: "some-branch-name",
+						},
+						BranchProtectionRules: BranchProtectionRules{
+							Nodes: []BranchProtectionRulesNodesList{{
+								Pattern: "some-branch-name",
+							}},
+						},
+					},
+				},
+			},
+			wantProblems: []string{"Test update error"},
+			returnError:  true,
+		},
+		{
+			name: "applyPrApprovalCreateError",
+			args: args{
+				repoSearchResult: map[string]RepositoriesNodeList{
+					"repo0": {
+						ID:            "repoIdTEST",
+						NameWithOwner: "org/some-repo-name",
+						DefaultBranchRef: DefaultBranchRef{
+							Name: "some-branch-name",
+						},
+					},
+				},
+			},
+			wantProblems: []string{"Test create error"},
+			returnError:  true,
+		},
 	}
-
-	
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.returnError {
+				prApprovalUpdate = mockedUpdatePrApprovalBranchProtectionError
+				prApprovalCreate = mockedCreatePrApprovalBranchProtectionError
+			}
+
 			gotModified, gotCreated, gotInfo, gotProblems := applyPrApproval(tt.args.repoSearchResult, tt.args.client)
 			if !reflect.DeepEqual(gotModified, tt.wantModified) {
 				t.Errorf("applyPrApproval() gotModified = %v, want %v", gotModified, tt.wantModified)
