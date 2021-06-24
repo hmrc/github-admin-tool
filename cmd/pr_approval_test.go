f35dbb325237384facfe7e51c401312db5924452
Masquerainsley <ainsley.gilligan@digital.hmrc.gov.uk>
2021-06-24 14:15:03 +0100

    PSEC-1208 - add test for PR approval flags

--- cmd/pr_approval_test.go
+++ cmd/pr_approval_test.go
@@ -1,1 +1,50 @@
 package cmd
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_setApprovalArgs(t *testing.T) {
+	tests := []struct {
+		name                     string
+		wantBranchProtectionArgs []BranchProtectionArgs
+	}{
+		{
+			name: "setApprovalargs returned values are as expected",
+			wantBranchProtectionArgs: []BranchProtectionArgs{
+				{
+					Name:     "requiresApprovingReviews",
+					DataType: "Boolean",
+					Value:    true,
+				},
+				{
+					Name:     "requiredApprovingReviewCount",
+					DataType: "Int",
+					Value:    1,
+				},
+				{
+					Name:     "dismissesStaleReviews",
+					DataType: "Boolean",
+					Value:    true,
+				},
+				{
+					Name:     "requiresCodeOwnerReviews",
+					DataType: "Boolean",
+					Value:    false,
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotBranchProtectionArgs := setApprovalArgs(); !reflect.DeepEqual(
+				gotBranchProtectionArgs,
+				tt.wantBranchProtectionArgs,
+			) {
+				t.Errorf("setApprovalArgs() = %v, want %v", gotBranchProtectionArgs, tt.wantBranchProtectionArgs)
+			}
+		})
+	}
+}
