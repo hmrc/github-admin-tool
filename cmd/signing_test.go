e4137dba3967fd7297c8de1cb56bf5a1a198d308
Masquerainsley <ainsley.gilligan@digital.hmrc.gov.uk>
2021-06-24 14:53:54 +0100

    PSEC-1208 add test for signing

--- cmd/signing_test.go
+++ cmd/signing_test.go
@@ -1,1 +1,34 @@
 package cmd
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_setSigningArgs(t *testing.T) {
+	tests := []struct {
+		name                     string
+		wantBranchProtectionArgs []BranchProtectionArgs
+	}{
+		{
+			name: "set SigningArgs return values are as expected",
+			wantBranchProtectionArgs: []BranchProtectionArgs{
+				{
+					Name:     "requiresCommitSignatures",
+					DataType: "Boolean",
+					Value:    true,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotBranchProtectionArgs := setSigningArgs(); !reflect.DeepEqual(
+				gotBranchProtectionArgs,
+				tt.wantBranchProtectionArgs,
+			) {
+				t.Errorf("setSigningArgs() = %v, want %v", gotBranchProtectionArgs, tt.wantBranchProtectionArgs)
+			}
+		})
+	}
+}
